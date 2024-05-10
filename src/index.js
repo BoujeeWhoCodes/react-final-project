5ec0e906277bb3faf2446603ef3992955cf25d1f
BoujeeWhoCodes <imellettis28@gmail.com>
2024-05-10 11:06:04 +0800

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { Provider } from 'react-redux';
+import store from './Store/index.js';
 
-import { configureStore } from '@reduxjs/toolkit';
-import weatherReducer from './Store/Slices/WeatherSlice';
-
-const store = configureStore({
-	reducer: {
-		weather: weatherReducer,
-	},
-});
-
-export default store;
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>
+);
