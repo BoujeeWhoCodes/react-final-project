9444333fceacd536cdcc91867ea50f92f74f68d1
BoujeeWhoCodes <imellettis28@gmail.com>
2024-05-15 18:02:55 +0800

    Update

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,10 +1,10 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import App from '../App.jsx';
 import './index.css';
 import { Provider } from 'react-redux';
-import store from './Store/index.js';
+import store from 'src/Store/index.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
